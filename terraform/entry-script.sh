d4e544100c5a029cb047306be22ab244d9578d07
Maximiliano Usich <maximilianou@gmail.com>
2021-03-05 12:06:43 -0300

    feat(prov): Provisioners is a workarround, do not use it

--- terraform/entry-script.sh
+++ terraform/entry-script.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-sudo yum -y update && sudo yum -y install docker
-sudo systemctl start docker
-sudo usermod -aG docker ec2-user
-docker run -p 8080:80 nginx
+sudo yum -y update && sudo yum -y install docker 
+sudo systemctl start docker 
+sudo usermod -aG docker ec2-user 
+/usr/local/bin/docker run -p 8080:80 nginx
--- terraform/main.tf
+++ terraform/main.tf
@@ -8,6 +8,7 @@ variable env_prefix {}
 variable my_ip {}
 variable instance_type {}
 variable my_public_key_location {}
+variable my_private_key_location {}
 resource "aws_vpc" "myapp-vpc" {
   cidr_block = var.vpc_cidr_block
   tags = { Name: "${var.env_prefix}-vpc" }
@@ -88,6 +89,23 @@ resource "aws_instance" "myapp-server" {
   availability_zone = var.avail_zone
   associate_public_ip_address = true
   key_name = aws_key_pair.ssh-key.key_name
-  user_data = file("entry-script.sh")
+  #user_data = file("entry-script.sh")
+  connection {
+    type = "ssh"
+    host = self.public_ip
+    user = "ec2-user"
+    private_key = file(var.my_private_key_location)
+  }
+  provisioner "file" {
+    source = "entry-script.sh"
+    destination = "/home/ec2-user/entry-script.sh"
+  }
+  provisioner "remote-exec" {
+    script = "entry-script.sh"
+  }
+  /*provisioner "local-exec" {
+    command = "echo ${self.public_ip} > output.txt"
+  }*/
   tags = { Name: "${var.env_prefix}-server" }
 }
+
